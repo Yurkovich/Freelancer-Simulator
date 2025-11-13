94ef9608ad30708903928c918778feed606a90d4
Yurkovich <yurkov200000@gmail.com>
2025-11-14 05:17:08 +0700

    Добавлена система окон и приложение Портфолио

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-console.log("Игра запущена")
-
 const icons = document.querySelectorAll(".icon")
+const portfolioWindow = document.getElementById("portfolio-window")
+const closeBtn = document.querySelector(".window-close")
 
 icons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    alert("Приложение пока не работает")
+  icon.addEventListener("click", (e) => {
+    const app = e.currentTarget.dataset.app
+
+    if (app === "portfolio") {
+      portfolioWindow.classList.remove("hidden")
+    } else {
+      alert("Приложение в разработке")
+    }
   })
+})
+
+closeBtn.addEventListener("click", () => {
+  portfolioWindow.classList.add("hidden")
 })
