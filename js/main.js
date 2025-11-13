b323cce7d5f89c141ca5d324300373ce3eb9c69e
Yurkovich <yurkov200000@gmail.com>
2025-11-14 05:01:48 +0700

    Базовая структура проекта с рабочим столом

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Игра запущена")
+
+const icons = document.querySelectorAll(".icon")
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    alert("Приложение пока не работает")
+  })
+})
